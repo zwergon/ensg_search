9914bc11c89d3f0d48fd971ef0fa59d556f2ce32
lecomtje <jean-francois.lecomte@ifpen.fr>
2025-02-07 18:37:10 +0100

    add pluv

---
+++ src/pluv_to_bulk.py
@@ -0,0 +1,59 @@
+import os
+import json
+import pandas as pd
+from datetime import datetime
+
+
+columns = [
+    "TIMESTAMP",
+    "RECORD",
+    "StationName",
+    "PluvioIntensityRT",
+    "PluvioAccRT_NRT",
+    "PluvioAccNRT",
+    "PluvioAccTotalNRT",
+    "PluvioBucketRT",
+    "PluvioBucketNRT",
+    "PluvioLoadCellTemp",
+    "PluvioHeatingStatus",
+    "PluvioStatus"
+]
+
+pluv_index = "pluvio"
+
+
+def to_bulk(root_path, out_file, n_files=100):
+
+    with open(out_file, "w") as bulk_file:
+
+        for i, f in enumerate(os.listdir(root_path)):
+            if n_files is not None and i + 1 > n_files:
+                return
+            if ".CR3" in f:
+                df = pd.read_csv(os.path.join(root_path, f),
+                                 sep=',', names=columns, skiprows=4)
+
+                for _, row in df.iterrows():
+
+                    bulk_file.write(json.dumps(
+                        {"index": {"_index": pluv_index}}) + "\n")
+
+                    _date = datetime.strptime(
+                        row['TIMESTAMP'], '%Y-%m-%d %H:%M:%S')
+                    print(_date)
+                    document = {
+                        "stno": int(row['StationName']),
+                        "time": _date.strftime('%Y-%m-%dT%H:%MZ'),
+                        "pluvio": float(row['PluvioBucketRT']),
+                        "temperature": float(row['PluvioLoadCellTemp'])
+                    }
+                    bulk_file.write(json.dumps(document) + "\n")
+
+
+if __name__ == "__main__":
+
+    root_path = os.path.join(os.path.dirname(__file__),
+                             "../data/pluvA/pluvA2025020613/")
+    out_file = os.path.join(os.path.dirname(
+        __file__), "../data/pluv_bulk.json")
+    to_bulk(root_path=root_path, out_file=out_file, n_files=100)
---
+++ src/practice.md
@@ -0,0 +1,5 @@
+# Index and handle GeoJSon data
+
+# Download dataset
+
+- Raw observations from climate stations (CAMP)[link](https://data.gov.ie/en_GB/dataset/raw-camp-station-data/resource/82b9a54c-dbfa-4d38-a989-04bf7ba01183)
