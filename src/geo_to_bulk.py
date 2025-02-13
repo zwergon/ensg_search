6fc7ddd228d34a7bd34b0ecbc7cc67d313ba1ebb
lecomtje <jean-francois.lecomte@ifpen.fr>
2025-02-13 09:46:00 +0100

    add python files

---
+++ src/geo_to_bulk.py
@@ -0,0 +1,40 @@
+import os
+import json
+from datetime import datetime
+
+if __name__ == "__main__":
+    directory =  os.path.join(os.path.dirname(__file__), "..", "data") 
+    in_file = os.path.join(directory,"campStationLocations.geojson")
+    out_file = os.path.join(directory,"bulk_data.json")
+
+    # Charger le fichier GeoJSON
+    with open(in_file, "r") as file:
+        geojson_data = json.load(file)
+
+    # Créer le fichier bulk
+    with open(out_file, "w") as bulk_file:
+        for feature in geojson_data["features"]:
+            # Préparer les données
+            properties = feature["properties"]
+            coordinates = feature["geometry"]["coordinates"]
+            
+            # Action metadata
+            bulk_file.write(json.dumps({ "index": { "_index": "geo_data" } }) + "\n")
+
+            dt = datetime.strptime(properties.get("open_date"), '%d/%m/%Y %H:%M')
+            
+            # Document data
+            document = {
+                "stno": properties.get("stno"),
+                "location": properties.get("location"),
+                "county": properties.get("County"),
+                "catchment": properties.get("catchment"),
+                "river": properties.get("river"),
+                "elevation": properties.get("elevation"),
+                "open_date": dt.strftime('%Y-%m-%dT%H:%MZ'),
+                "coordinates": {
+                    "lat": coordinates[1],
+                    "lon": coordinates[0]
+                }
+            }
+            bulk_file.write(json.dumps(document) + "\n")
